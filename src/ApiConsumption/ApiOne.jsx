bde2f6366d6f00e5ebec5347d14c01adbfc09492
IkeoluwaAshade <91098490+IkeoluwaAshade@users.noreply.github.com>
2024-06-07 04:20:03 +0100

    Updates

--- src/ApiConsumption/ApiOne.jsx
+++ src/ApiConsumption/ApiOne.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react'
 import axios from "axios"
 import { Link } from "react-router-dom"
 import styled from 'styled-components'
+import { VscProject } from 'react-icons/vsc';
+import { IoCloudDownloadSharp } from "react-icons/io5";
+import { FaCommentDots } from "react-icons/fa";
+import { FaCar } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 
 const ApiOne = () => {
     const [getData, setGetData] = useState([])
@@ -19,20 +24,37 @@ const ApiOne = () => {
 
   return (
     <CardContainer>
+        
         {getData.map((myData) => (
             <Card key={myData.id}>
+                <VscProject size={'25px'}/>
                 <p>
                     <b>Title</b>: {myData.title}
                 </p>
+                <Hashtag>
+                    <p>#Web Development</p>
+                </Hashtag>
                 <p>
                     <b>Price</b>: ${myData.price}
                 </p>
-                <div>
-                    <img src={myData.image} alt="" />
+                <TimeandDate>
+                    <p>June 06 - 8m watch time</p>
+                </TimeandDate>
+                <div> 
+                    <Link to={`/detail/${myData.id}`}>
+                        <img src={myData.image} alt="" />
+                    </Link>
                 </div>
                 <Link to={`/detail/${myData.id}`}>
                     <button>View More</button>
                 </Link>
+                <Analysis>
+                    <Downloads><IoCloudDownloadSharp /><p>278</p></Downloads>
+                    <Comment><FaCommentDots />
+                    27</Comment>
+                    <Save><FaCar /></Save>
+                    <LinkIcon><FaLink /></LinkIcon>
+                </Analysis>
             </Card>
         ))}
     </CardContainer>
@@ -61,6 +83,7 @@ const Card = styled.div`
         height: 200px;
         width: 200px;
         background-color: gray;
+        cursor: pointer;
         /* object-fit: cover; */
     }
 
@@ -83,4 +106,58 @@ const Card = styled.div`
         }
     }
 
+`
+
+const Hashtag = styled.div`
+    background-color: #fff;
+    width: 140px;
+    border: 2px solid gray;
+    border-radius: 7px;
+    font-size: 14px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`
+
+const TimeandDate = styled.div`
+    font-size: 10px;
+    font-weight: 700;
+`
+
+const Analysis = styled.div`
+    /* background-color: red; */
+    width: 200px;
+    margin-top: 12px;
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Downloads = styled.div`
+    /* background-color: green; */
+    width: 51px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+`
+
+const Comment = styled.div`
+    /* background-color: brown; */
+    width: 51px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+`
+
+const Save = styled.div`
+    cursor: pointer;
+`
+
+const LinkIcon =  styled.div`
+    cursor: pointer;
 `
